6433f44bb6bf337129c2648d4690047771d86e26
B-singularity <skil1489@naver.com>
2024-02-29 18:40:08 +0900

    singly circular linkedList

---
+++ src/list/CircularSinglyLinkedList.java
@@ -0,0 +1,48 @@
+package list;
+
+public class CircularSinglyLinkedList {
+
+	private ListNode last;
+	private int length;
+
+	private static class ListNode {
+		private ListNode next;
+		private int data;
+
+		public ListNode(int data) {
+			this.data = data;
+		}
+	}
+
+	public CircularSinglyLinkedList() {
+		last = null;
+		length = 0;
+	}
+
+	public int length() {
+		return length;
+	}
+
+	public boolean isEmpty() {
+		return length == 0;
+	}
+
+	public void creatCircularLinkedList() {
+		ListNode first = new ListNode(1);
+		ListNode second = new ListNode(5);
+		ListNode third = new ListNode(10);
+		ListNode fourth = new ListNode(15);
+
+		first.next = second;
+		second.next = third;
+		third.next = fourth;
+		fourth.next = first;
+
+		last = fourth;
+	}
+
+	public static void main(String[] args) {
+		CircularSinglyLinkedList circularSinglyLinkedList = new CircularSinglyLinkedList();
+		circularSinglyLinkedList.creatCircularLinkedList();
+	}
+}
