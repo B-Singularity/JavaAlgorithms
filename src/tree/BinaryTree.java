0b45a205e27bd10ae620792f4508e2db2ccfd165
B-singularity <skil1489@naver.com>
2024-03-08 19:14:18 +0900

    postorder Traversal of binaryTree

--- src/tree/BinaryTree.java
+++ src/tree/BinaryTree.java
@@ -70,6 +70,29 @@ public class BinaryTree {
         }
     }
 
+    public void postOrder(TreeNode root) {
+        TreeNode current = root;
+        Stack<TreeNode> stack = new Stack<>();
+
+        while (current != null || !stack.isEmpty()) {
+            if (current != null) {
+                stack.push(current);
+                current = current.left;
+            } else {
+                TreeNode temp = stack.peek().right;
+                if (temp == null) {
+                    temp = stack.pop();
+                    System.out.print(temp.data + " ");
+                    while (!stack.isEmpty() && temp == stack.peek().right) {
+                        temp = stack.pop();
+                        System.out.println(temp.data + " ");
+                    }
+                } else {
+                    current = temp;
+                }
+            }
+        }
+    }
 
     public static void main(String[] args) {
         BinaryTree bt = new BinaryTree();
