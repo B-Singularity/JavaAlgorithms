51921db94677dc7a2962787ffcb7b78bb7c0c1c5
B-singularity <skil1489@naver.com>
2024-03-09 16:10:20 +0900

    insert value in binary Search Tree

--- src/tree/BinaryTree.java
+++ src/tree/BinaryTree.java
@@ -1,5 +1,7 @@
 package tree;
 
+import java.util.LinkedList;
+import java.util.Queue;
 import java.util.Stack;
 
 public class BinaryTree {
@@ -94,9 +96,53 @@ public class BinaryTree {
         }
     }
 
+    public void levelOrder() {
+        if (root == null) {
+            return;
+        }
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.offer(root);
+
+        while (!queue.isEmpty()) {
+            TreeNode temp = queue.poll();
+            System.out.print(temp.data + " ");
+            if (temp.left != null) {
+                queue.offer(temp.left);
+            }
+            if (temp.right != null) {
+                queue.offer(temp.right);
+            }
+        }
+    }
+
+    public int findMax() {
+        return findMax(root);
+    }
+
+    public int findMax(TreeNode root) {
+        if (root == null) {
+            return Integer.MIN_VALUE;
+        }
+
+        int result = root.data;
+        int left = findMax(root.left);
+        int right = findMax(root.right);
+
+        if (left > result) {
+            result = left;
+        }
+
+        if (right > result) {
+            result = right;
+        }
+
+        return result;
+    }
+
     public static void main(String[] args) {
         BinaryTree bt = new BinaryTree();
         bt.createBinaryTree();
-        bt.preOrder();
+        bt.levelOrder();
+        System.out.println("\n" + bt.findMax());
     }
 }
