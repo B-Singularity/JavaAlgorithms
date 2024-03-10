acd22bef6d4434d8cfc208fde3ee814b64bac09d
B-singularity <skil1489@naver.com>
2024-03-10 16:18:12 +0900

    LinearSearch

---
+++ src/search/LinearSearch.java
@@ -0,0 +1,19 @@
+package search;
+
+public class LinearSearch {
+
+    public int search(int[] arr, int n, int x) {
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] == x) {
+                return i;
+            }
+        }
+        return -1;
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {5, 1, 9, 2, 10, 15, 20};
+        LinearSearch linearSearch = new LinearSearch();
+        System.out.println(linearSearch.search(arr, arr.length, 5));
+    }
+}
