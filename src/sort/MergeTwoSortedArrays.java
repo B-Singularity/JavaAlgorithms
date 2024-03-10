37d6b0a3d3aff3442f7715b2db0ffc3e437e1b20
B-singularity <skil1489@naver.com>
2024-03-10 17:40:37 +0900

    Merging two sorted arrays

---
+++ src/sort/MergeTwoSortedArrays.java
@@ -0,0 +1,54 @@
+package sort;
+
+public class MergeTwoSortedArrays {
+
+    public int[] merge(int[] arr1, int[] arr2, int n, int m) {
+        int[] result = new int[n + m];
+        int i = 0; //traverse arr1
+        int j = 0; //traverse arr2;
+        int k = 0; //traverse arr3;
+
+        while (i < n && j < m) {
+            if (arr1[i] < arr2[j]) {
+                result[k] = arr1[i];
+                i++;
+            } else {
+                result[k] = arr2[j];
+                j++;
+            }
+            k++;
+        }
+        while (i < n) {
+            result[k] = arr1[i];
+            i++;
+            k++;
+        }
+
+        while (j < m) {
+            result[k] = arr2[j];
+            j++;
+            k++;
+        }
+
+        return result;
+    }
+
+    public void printArray(int[] arr) {
+        int n = arr.length;
+        for (int i = 0; i < n; i++) {
+            System.out.println(arr[i] + " ");
+        }
+        System.out.println();
+    }
+
+    public static void main(String[] args) {
+        MergeTwoSortedArrays msa = new MergeTwoSortedArrays();
+        int[] arr1 = {0, 1, 8, 10};
+        int[] arr2 = {2, 4, 11, 15, 20};
+        msa.printArray(arr1);
+        msa.printArray(arr2);
+
+        int[] result = msa.merge(arr1, arr2, arr1.length, arr2.length);
+        msa.printArray(result);
+    }
+}
