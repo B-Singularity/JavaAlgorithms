7bda00ab8527729e3a059bb69f7acbdbfab1cbf8
B-singularity <skil1489@naver.com>
2024-03-15 19:00:34 +0900

    bfs

--- src/graph/Graph.java
+++ src/graph/Graph.java
@@ -1,29 +1,35 @@
 package graph;
 
+import java.util.LinkedList;
+import java.util.Queue;
+
 public class Graph {
 
+    private LinkedList<Integer>[] adj;
     private int V;
     private int E;
-    private int[][] adjMatrix;
 
     public Graph(int nodes) {
         this.V = nodes;
         this.E = 0;
-        this.adjMatrix = new int[nodes][nodes];
+        this.adj = new LinkedList[nodes];
+        for (int v = 0; v < V; v++) {
+            adj[v] = new LinkedList<>();
+        }
     }
 
     public void addEdge(int u, int v) {
-        adjMatrix[u][v] = 1;
-        adjMatrix[v][u] = 1;
+        adj[u].add(v);
+        adj[v].add(u);
         E++;
     }
 
     public String toString() {
         StringBuilder sb = new StringBuilder();
-        sb.append(V + " vertices,  " + E + " edges " + "\n");
+        sb.append(V + " vertices, " + E + " edges " + "\n");
         for (int v = 0; v < V; v++) {
-            sb.append(v + ": ");
-            for (int w : adjMatrix[v]) {
+            sb.append(V + ": ");
+            for (int w : adj[v]) {
                 sb.append(w + " ");
             }
             sb.append("\n");
@@ -31,12 +37,34 @@ public class Graph {
         return sb.toString();
     }
 
+    public void bfs(int s) {
+        boolean[] visited = new boolean[V];
+
+        Queue<Integer> q = new LinkedList<>();
+        visited[s] = true;
+        q.offer(s);
+
+        while (!q.isEmpty()) {
+            int u = q.poll();
+            System.out.print(u + " ");
+
+            for (int v : adj[u]) {
+                if (!visited[v]) {
+                    visited[v] = true;
+                    q.offer(v);
+                }
+            }
+        }
+    }
+
     public static void main(String[] args) {
-        Graph g = new Graph(4);
+        Graph g = new Graph(5);
         g.addEdge(0, 1);
         g.addEdge(1, 2);
         g.addEdge(2, 3);
         g.addEdge(3, 0);
-        System.out.println(g);
+        g.addEdge(2, 4);
+        g.bfs(0);
     }
 }
+
